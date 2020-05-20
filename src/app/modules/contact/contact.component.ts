85c42e49bdf8758891de3cf2e2f14fb058cd83a5
akshay <akshaykarajgikar@gmail.com>
2020-05-20 22:16:43 +0530

    Contacts -> Email Copy functionality added
    Projects -> Project status visibility corrected

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -11,4 +11,22 @@ export class ContactComponent implements OnInit {
   public ngOnInit(): void {
     setTimeout(() => (this.showContent = true), 1000);
   }
+
+  copyText(val: string) {
+    let selBox = document.createElement("textarea");
+    selBox.style.position = "fixed";
+    selBox.style.left = "0";
+    selBox.style.top = "0";
+    selBox.style.opacity = "0";
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand("copy");
+    document.body.removeChild(selBox);
+    document.querySelector("#email").innerHTML = "Copied";
+    setTimeout(() => {
+      document.querySelector("#email").innerHTML = "Copy";
+    }, 3000);
+  }
 }
